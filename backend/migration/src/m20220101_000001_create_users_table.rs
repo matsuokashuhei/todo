8c9020dd1a131b871ba15d4df2ba001dd174c560
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-10-02 16:41:32 +0000

    Add `tasks` table

--- backend/migration/src/m20220101_000001_create_users_table.rs
+++ backend/migration/src/m20220101_000001_create_users_table.rs
@@ -1,5 +1,7 @@
-use chrono::{DateTime, NaiveDateTime, Utc};
-use sea_orm_migration::prelude::*;
+use sea_orm_migration::{
+    prelude::*,
+    sea_orm::{ConnectionTrait, Statement},
+};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -7,31 +9,43 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .create_table(
-                Table::create()
-                    .table(Users::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(Users::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
-                    )
-                    .col(ColumnDef::new(Users::Name).string().not_null())
-                    // .col(ColumnDef::new(Users::CreatedAt).timestamp().not_null())
-                    // .col(ColumnDef::new(Users::UpdatedAt).timestamp().not_null())
-                    .to_owned(),
-            )
-            .await
+        // manager
+        //     .create_table(
+        //         Table::create()
+        //             .table(Users::Table)
+        //             .if_not_exists()
+        //             .col(
+        //                 ColumnDef::new(Users::Id)
+        //                     .integer()
+        //                     .not_null()
+        //                     .auto_increment()
+        //                     .primary_key(),
+        //             )
+        //             .col(ColumnDef::new(Users::Name).string().not_null())
+        //             // .col(ColumnDef::new(Users::CreatedAt).timestamp().not_null())
+        //             // .col(ColumnDef::new(Users::UpdatedAt).timestamp().not_null())
+        //             .to_owned(),
+        //     )
+        //     .await
+        let sql = r#"
+        CREATE TABLE `users` (
+            `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `name` varchar(255) NOT NULL,
+            `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )
+        "#;
+        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
+        manager.get_connection().execute(stmt).await.map(|_| ())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        manager
-            .drop_table(Table::drop().table(Users::Table).to_owned())
-            .await
+        // manager
+        //     .drop_table(Table::drop().table(Users::Table).to_owned())
+        //     .await
+        let sql = "DROP TABLE IF EXISTS `users`";
+        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
+        manager.get_connection().execute(stmt).await.map(|_| ())
     }
 }
 
---
+++ backend/migration/src/m20221002_115406_create_tasks_table.rs
@@ -0,0 +1,42 @@
+use sea_orm_migration::{
+    prelude::*,
+    sea_orm::{ConnectionTrait, Statement},
+};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let sql = r#"
+        CREATE TABLE `tasks` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `user_id` int NOT NULL,
+            `title` varchar(255) NOT NULL,
+            `description` text,
+            `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`),
+            FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+        )
+        "#;
+        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
+        manager.get_connection().execute(stmt).await.map(|_| ())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let sql = "DROP TABLE IF EXISTS `tasks`";
+        let stmt = Statement::from_string(manager.get_database_backend(), sql.to_owned());
+        manager.get_connection().execute(stmt).await.map(|_| ())
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Post {
+    Table,
+    Id,
+    Title,
+    Text,
+}
